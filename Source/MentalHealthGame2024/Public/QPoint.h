b22994df806be7f1291184ac47d264b99c940158
Han Nguyen <nguye340@sheridancollege.ca>
2024-06-18 11:25:00 -0400

    new codes for gesture recognition

---
+++ Source/MentalHealthGame2024/Public/QPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "QPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MENTALHEALTHGAME2024_API UQPoint : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};

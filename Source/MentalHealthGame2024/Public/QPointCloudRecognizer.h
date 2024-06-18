2c608f4a99ffb6d2e9c0c789b63409382279fe8d
Han Nguyen <nguye340@sheridancollege.ca>
2024-06-18 13:04:05 -0400

    add gesture recognition logic

---
+++ Source/MentalHealthGame2024/Public/QPointCloudRecognizer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "QPointCloudRecognizer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MENTALHEALTHGAME2024_API UQPointCloudRecognizer : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};

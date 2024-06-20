220ea476fea1ac838a120833ae3439be6ed8bf8f
Han Nguyen <nguye340@sheridancollege.ca>
2024-06-20 12:26:06 -0400

    can call spell manager C++ code in BP

---
+++ Source/MentalHealthGame2024/Public/SpellManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SpellManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MENTALHEALTHGAME2024_API USpellManager : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};

8ed24c15c1e66491350c055caf7690941f66f215
Han Nguyen <nguye340@sheridancollege.ca>
2024-09-21 11:52:01 -0400

    add data table for effect tags

--- Source/MentalHealthGame2024/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/MentalHealthGame2024/Public/UI/WidgetController/OverlayWidgetController.h
@@ -3,10 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "UI/WidgetController/AlmaWidgetController.h"
+#include "Engine/DataTable.h"
 
 #include "OverlayWidgetController.generated.h"
 
+class UAlmaUserWidget;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
@@ -15,6 +18,25 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, N
 /**
  * 
  */
+USTRUCT(BlueprintType)
+struct MENTALHEALTHGAME2024_API FUIWidgetRow : public FTableRowBase
+
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)	
+	FGameplayTag MessageAssetTag = FGameplayTag();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText Message = FText();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<UAlmaUserWidget> MessageWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* Image = nullptr;
+};
+
 struct FOnAttributeChangeData;
 UCLASS(BlueprintType, Blueprintable)
 class MENTALHEALTHGAME2024_API UOverlayWidgetController : public UAlmaWidgetController
